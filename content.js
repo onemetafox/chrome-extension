43a0d7d3bb0161ac007ab6ca5a4b30569b05a6df
todaycodemaster <seniordev1994128@gmail.com>
2023-06-06 02:52:24 -0700

    update code to get the dom content

--- content.js
+++ content.js
@@ -2,3 +2,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log('from background script: ' + message )
     sendResponse('message recieved')
 });
+
+function sendToBackground(type, data) {
+    chrome.runtime.sendMessage({
+        type,
+        target: 'background',
+        data
+    });
+}
--- popup.js
+++ popup.js
@@ -43,11 +43,11 @@ for (const tab of tabs) {
 }
 document.querySelector('ul').append(...elements);
 
-const button = document.querySelector('button');
-button.addEventListener('click', async () => {
-  const tabIds = tabs.map(({ id }) => id);
-  if (tabIds.length) {
-    const group = await chrome.tabs.group({ tabIds });
-    await chrome.tabGroups.update(group, { title: 'DOCS' });
-  }
-});
+// const button = document.querySelector('button');
+// button.addEventListener('click', async () => {
+//   const tabIds = tabs.map(({ id }) => id);
+//   if (tabIds.length) {
+//     const group = await chrome.tabs.group({ tabIds });
+//     await chrome.tabGroups.update(group, { title: 'DOCS' });
+//   }
+// });
